13ae61bb44970e4b7205a0f8c46ccf16cfc37971
Khanh Pham <khanhpdtvn@gmail.com>
2020-04-21 00:05:19 +0700

    Add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import './App.scss';
+import React from "react"
+import "./App.scss"
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      fileName: null
+      fileName: null,
     }
   }
 
-  onUpload(){ 
-    console.log('upload');
+  onUpload() {
+    console.log("upload")
   }
 
   onChangeFile(event) {
-    console.log(event);
+    console.log(event)
   }
 
   render() {
-    const fileName = this.state.fileName ? (<span class="file-name">{this.state.fileName}</span>) : null;
+    const fileName = this.state.fileName ? (
+      <span class="file-name">{this.state.fileName}</span>
+    ) : null
     return (
       <div>
         <div className="file is-centered has-name">
           <label className="file-label">
-            <input className="file-input" type="file" onChange={(event) => this.onChangeFile(event)}/>
+            <input
+              className="file-input"
+              type="file"
+              onChange={(event) => this.onChangeFile(event)}
+            />
             <span className="file-cta">
               <span className="file-icon">
                 <i className="fas fa-upload"></i>
@@ -33,13 +38,13 @@ class App extends React.Component {
             </span>
             {fileName}
           </label>
-          <button className="button is-info" onClick={() => this.onUpload()}>Upload</button>
+          <button className="button is-info" onClick={() => this.onUpload()}>
+            Upload
+          </button>
         </div>
       </div>
-    );
+    )
   }
-
 }
 
-
-export default App;
+export default App
