0861f97afedf0f7f90346a88bd6fcd3fe31e1ac2
Khanh Pham <khanhpdtvn@gmail.com>
2020-05-14 00:03:01 +0700

    Fis misalign text layer (see react-pdf issue 332)

--- src/components/PdfView/component.js
+++ src/components/PdfView/component.js
@@ -2,11 +2,26 @@ import React from "react"
 import { Document, Page } from "react-pdf"
 import "./component.scss"
 
+function removeTextLayerOffset() {
+  const textLayers = document.querySelectorAll(".react-pdf__Page__textContent")
+  textLayers.forEach((layer) => {
+    const { style } = layer
+    style.top = "0"
+    style.left = "0"
+    style.transform = ""
+  })
+}
+
 export default function PdfView({ src }) {
   return (
     <div>
       <Document file={src} className="pdf-document">
-        <Page pageNumber={3} renderMode="svg" className="pdf-page-svg" />
+        <Page
+          pageNumber={3}
+          renderMode="svg"
+          className="pdf-page-svg"
+          onLoadSuccess={removeTextLayerOffset}
+        />
       </Document>
       <p>Page 3</p>
     </div>
