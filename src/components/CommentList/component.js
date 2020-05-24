bc159a2070d00d2b533ff6d0b79f625a677aba65
Khanh Pham <khanhpdtvn@gmail.com>
2020-05-24 18:06:40 +0700

    Init comment feature

--- src/components/CommentList/component.js
+++ src/components/CommentList/component.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function CommentList() {
-  return <div>Comment list</div>
+  return (
+    <div>
+      <h4 className="title is-4">Comments</h4>
+      <div>
+        <button className="button">Add comment</button>
+      </div>
+    </div>
+  )
 }
